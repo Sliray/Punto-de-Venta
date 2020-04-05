3f2073d2b1bb803c8f65ad6d5cf4a63f7c70cb30
Sebas <sebastianrnoriega.hotmail.com>
2020-04-05 16:48:55 -0700

    Diseño

---
+++ src/punto/de/venta/java/PuntoDeVentaJava.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto.de.venta.java;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sebas
+ */
+public class PuntoDeVentaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Interfaz ventas = new Interfaz();
+        ventas.pack();
+        ventas.setLocationRelativeTo(null);
+        ventas.setExtendedState(ventas.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        ventas.setVisible(true);
+        
+    }
+    
+}
